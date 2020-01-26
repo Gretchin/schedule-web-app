297d9d7cd2468c46e17e982560f58d1f3caf6caf
Gretchin <uru-aleksandr@mail.ru>
2020-01-26 11:41:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
-
 import Timetable from "./components/timetable";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <CssBaseline />
-      {<Timetable />}
-    </React.Fragment>
+    // хз зачем скобки
+      <Timetable />
+    </>
   );
 }
 
